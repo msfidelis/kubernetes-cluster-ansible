1f9047ab05d97182985351b2e9cd90159dddcf47
Matheus Fidelis <msfidelis01@gmail.com>
2019-03-25 23:37:31 -0300

    vai

--- terraform/modules/cluster/etcd.tf
+++ terraform/modules/cluster/etcd.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "etcd-a" {
 
     associate_public_ip_address = true
 
-    vpc_security_group_ids = ["${aws_security_group.etcd_sg.id}"]
+    vpc_security_group_ids = ["${aws_security_group.cluster_sg.id}"]
 
     key_name = "${var.cluster_key}"
 
@@ -17,82 +17,4 @@ resource "aws_instance" "etcd-a" {
         Workload    = "kubernetes"
         Role        = "master"
     }
-}
-
-// resource "aws_instance" "etcd-b" {
-//     count = 1
-//     ami = "ami-0a313d6098716f372"
-//     instance_type = "t2.micro"
-
-//     subnet_id = "${var.public_subnet_1b}"
-
-//     associate_public_ip_address = true
-
-//     vpc_security_group_ids = ["${aws_security_group.etcd_sg.id}"]
-
-//     key_name = "${var.cluster_key}"
-
-//     tags {
-//         Name        = "${var.cluster_name}-master"
-//         Workload    = "kubernetes"
-//         Role        = "master"
-//     }
-// }
-
-
-resource "aws_security_group" "etcd_sg" {
-
-  name        = "${var.cluster_name}-etcd-sg"
-  description = "${var.cluster_name} cluster"
-  vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port   = "22"
-    to_port     = "22"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {   
-      from_port = 0
-      to_port = 65535
-      protocol = "tcp"
-      security_groups = ["${aws_security_group.nodes_sg.id}"]
-      description = "nodes"
-  }  
-
-  ingress {   
-      from_port = 0
-      to_port = 65535
-      protocol = "udp"
-      security_groups = ["${aws_security_group.nodes_sg.id}"]
-      description = "nodes"
-  }    
-
-  ingress {   
-      from_port = 0
-      to_port = 65535
-      protocol = "tcp"
-      self = true
-      description = "master"
-  }    
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "${var.cluster_name}-etcd-sg"
-  }
-
 }
--- terraform/modules/cluster/nodes.tf
+++ terraform/modules/cluster/nodes.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "nodes-a" {
 
     associate_public_ip_address = true
 
-    vpc_security_group_ids = ["${aws_security_group.nodes_sg.id}"]
+    vpc_security_group_ids = ["${aws_security_group.cluster_sg.id}"]
 
     key_name = "${var.cluster_key}"
 
@@ -28,7 +28,7 @@ resource "aws_instance" "nodes-b" {
 
     associate_public_ip_address = true
 
-    vpc_security_group_ids = ["${aws_security_group.nodes_sg.id}"]
+    vpc_security_group_ids = ["${aws_security_group.cluster_sg.id}"]
 
     key_name = "${var.cluster_key}"
 
@@ -38,72 +38,3 @@ resource "aws_instance" "nodes-b" {
         Role = "node"
     }
 }
-
-resource "aws_security_group" "nodes_sg" {
-
-  name        = "${var.cluster_name}-nodes-sg"
-  description = "${var.cluster_name} cluster"
-  vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port   = "22"
-    to_port     = "22"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {   
-      from_port = 0
-      to_port = 65535
-      protocol = "tcp"
-      self = true
-      description = "nodes"
-  }    
-
-  ingress {   
-      from_port = 0
-      to_port = 65535
-      protocol = "udp"
-      self = true
-      description = "nodes"
-  }     
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "${var.cluster_name}-nodes-sg"
-  }
-
-}
-
-resource "aws_security_group_rule" "extra_rule_master_tcp" {
-    security_group_id        = "${aws_security_group.nodes_sg.id}"
-    from_port                = 0
-    to_port                  = 65535
-    protocol                 = "tcp"
-    type                     = "ingress"
-    description              = "master"
-    source_security_group_id = "${aws_security_group.etcd_sg.id}"
-}
-
-// resource "aws_security_group_rule" "extra_rule_master_udp" {
-//     security_group_id        = "${aws_security_group.nodes_sg.id}"
-//     from_port                = 0
-//     to_port                  = 65535
-//     protocol                 = "udp"
-//     type                     = "ingress"
-//     description              = "master"
-//     source_security_group_id = "${aws_security_group.etcd_sg.id}"
-// }
